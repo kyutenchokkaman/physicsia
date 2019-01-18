3146e4570ed8007f129be8dbe842e04e8c10f5b3
kyutenchokkaman <nekotokotatsu.nyaaaaa@gmail.com>
2019-01-18 20:28:36 +0900

    hai

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ import matplotlib.pyplot as plt
 import numpy as np
 #グラフを描く
 
-filename = "hai.csv"
+filename = "n=2data.csv"
 raw_data = open(filename, 'r')
 data = np.loadtxt(raw_data, delimiter=",")
 
@@ -14,25 +14,30 @@ x = []
 y = []
 
 for i in data:
-  x.append(i[0])
-for i in data:
-  y.append(i[1])
+ if i[4] < 1.0:
+  x.append(i[4])
+ else:
+  x.append(0)
 
-plt.plot(x,y,'bo',label = "rand")
+for i in data:
+  y.append(i[3])
+
+func = np.poly1d(np.polyfit(x,y,2))(x)
+
+plt.plot(x,y,'go',label = "points")
+plt.plot(x, func, label='Least Squares Method')
 
 err = []
-#誤差棒を描く
 for i in y:
     ev = errorBar(i, 0.025)
     err.append(ev)
 
-r = np.corrcoef(x,y)[0][1]
-
-plt.errorbar(x,y,yerr=err,fmt='ro',ecolor='g')
+r = np.corrcoef(x,y)
+plt.errorbar(x,y,yerr=err,fmt='go',ecolor='g')
 
 plt.legend()
-plt.title("figure 1.1")
-plt.xlabel("x")
-plt.ylabel("Y")
-plt.axis([0,1,0,1])
+plt.title("figure 2.5")
+plt.xlabel("Induced Current (μA)")
+plt.ylabel("Velocity of Bar Magnet (cm/s)")
+plt.axis([0,4,5,30])
 plt.show()
