4695383d32fba437fddc2d7c150e73e441e70c15
kyutenchokkaman <nekotokotatsu.nyaaaaa@gmail.com>
2019-01-28 22:42:09 +0900

    added csvs

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ import matplotlib.pyplot as plt
 import numpy as np
 #グラフを描く
 
-filename = "n=2data.csv"
+filename = "data_macro.csv"
 raw_data = open(filename, 'r')
 data = np.loadtxt(raw_data, delimiter=",")
 
@@ -22,7 +22,7 @@ for i in data:
 for i in data:
   y.append(i[3])
 
-func = np.poly1d(np.polyfit(x,y,2))(x)
+func = np.poly1d(np.polyfit(x,y,1))(x)
 
 plt.plot(x,y,'go',label = "points")
 plt.plot(x, func, label='Least Squares Method')
@@ -41,3 +41,10 @@ plt.xlabel("Induced Current (μA)")
 plt.ylabel("Velocity of Bar Magnet (cm/s)")
 plt.axis([0,4,5,30])
 plt.show()
+
+#calculated theoretical value
+'''x = np.linspace(0.1, 30)
+y = -1.502*10**-6/x
+
+plt.plot(x,y)
+plt.show()'''
